61abc42613cc70707eda3d3beeb9dfb5c79e14d1
MinexAutomation <minex.automation@gmail.com>
2019-12-09 15:44:10 -0800

    Added IServiceCollection extensions to add database configuration services.

---
+++ source/R5T.Portsmouth/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Sardinia;
+
+using R5T.Portsmouth.Configuration;
+
+using RawDatabaseConfiguration = R5T.Portsmouth.Configuration.Raw.DatabaseConfiguration;
+
+
+namespace R5T.Portsmouth
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services)
+        {
+            services
+                .Configure<RawDatabaseConfiguration>()
+                .ConfigureOptions<DatabaseConfigurationConfigureOptions>()
+                ;
+
+            return services;
+        }
+    }
+}
