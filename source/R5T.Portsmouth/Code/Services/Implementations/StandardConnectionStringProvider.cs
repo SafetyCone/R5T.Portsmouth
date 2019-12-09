78bec0d7a0835b83feb794f877eaa18d86d3a2b7
MinexAutomation <minex.automation@gmail.com>
2019-12-09 13:02:15 -0800

    Implemented standard connection string provider.

---
+++ source/R5T.Portsmouth/Code/Services/Implementations/StandardConnectionStringProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Microsoft.Extensions.Options;
+
+using R5T.Anactorium;
+using R5T.Lincoln;
+
+using R5T.Portsmouth.Configuration;
+
+
+namespace R5T.Portsmouth
+{
+    public class StandardConnectionStringProvider : IConnectionStringProvider
+    {
+        private IOptions<DatabaseConfiguration> DatabaseConfiguration { get; }
+        private IOptions<DatabaseServerAuthentications> DatabaseServerAuthentications { get; }
+
+
+        public StandardConnectionStringProvider(IOptions<DatabaseConfiguration> databaseConfiguration, IOptions<DatabaseServerAuthentications> databaseServerAuthentications)
+        {
+            this.DatabaseConfiguration = databaseConfiguration;
+            this.DatabaseServerAuthentications = databaseServerAuthentications;
+        }
+
+        public string GetConnectionString()
+        {
+            var databaseConfiguration = this.DatabaseConfiguration.Value;
+            var databaseServerAuthentications = this.DatabaseServerAuthentications.Value;
+
+            var connectionString = databaseConfiguration.GetConnectionString(databaseServerAuthentications);
+
+            var output = connectionString.Value;
+            return output;
+        }
+    }
+}
